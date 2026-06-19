ea4f354cbf71e4e23ac63349ce73924b26a229dc
angubofrancis1-ai <angubofrancis1@gmail.com>
2026-06-19 11:01:25 +0300

    Add navigation buttons for messages and settings

---
+++ components/app/messages/app/settings/app/page.tsx
@@ -0,0 +1,6 @@
+<button onClick={() => window.location.href = '/messages'}>
+  <Mail size={28} />
+</button>
+<button onClick={() => window.location.href = '/settings'}>
+  <User size={28} />
+</button>
